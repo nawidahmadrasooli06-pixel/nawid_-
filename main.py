93e1a47867ebf99d98d2e048c4bb73201522fa03
Nawid Azimi <nawidahmadrasooli06@gmail.com>
2026-09-20 20:29:22 +0200

    Add new handlers and update existing ones

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 from config import BOT_TOKEN
 from database import is_blocked
 from handlers.start import start_command, language_callback, menu_callback
-from handlers.owner import stars_toggle_callback, preview_callback, day_callback, timezone_callback
+from handlers.owner import stars_toggle_callback, preview_callback, day_callback, timezone_callback, calendar_callback, emoji_callback
 from handlers.like import like_callback, chat_member_update
 from handlers.about import about_callback
 from handlers.stars import message_reaction_count_update
 from handlers.countdown import start_scheduler
-from handlers.router import text_router, photo_router
+from handlers.router import text_router, photo_router, general_nontext_router
 from handlers.admin import stats_command, block_command
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
@@ -45,19 +45,22 @@ def create_application():
     app.add_handler(CommandHandler("block", block_command))
     app.add_handler(CallbackQueryHandler(language_callback, pattern=r"^lang_(fa|en)$"))
     app.add_handler(CallbackQueryHandler(stars_toggle_callback, pattern=r"^stars_(yes|no)$"))
-    app.add_handler(CallbackQueryHandler(preview_callback, pattern=r"^preview_(confirm|cancel)$"))
+    app.add_handler(CallbackQueryHandler(preview_callback, pattern=r"^preview_(confirm|cancel|emoji|back)$"))
     app.add_handler(CallbackQueryHandler(day_callback, pattern=r"^owner_day_"))
     app.add_handler(CallbackQueryHandler(timezone_callback, pattern=r"^tz_(af|ir|de)$"))
+    app.add_handler(CallbackQueryHandler(calendar_callback, pattern=r"^calendar_(gregorian|iran|afghan)$"))
+    app.add_handler(CallbackQueryHandler(emoji_callback, pattern=r"^emoji_(add|continue)$"))
     app.add_handler(CallbackQueryHandler(like_callback, pattern=r"^like_"))
     app.add_handler(CallbackQueryHandler(menu_callback))
     app.add_handler(MessageHandler(filters.PHOTO, photo_router))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_router))
+    app.add_handler(MessageHandler(~filters.COMMAND & ~filters.TEXT & ~filters.PHOTO, general_nontext_router))
     app.add_handler(ChatMemberHandler(chat_member_update, ChatMemberHandler.CHAT_MEMBER))
     app.add_handler(MessageReactionHandler(message_reaction_count_update, message_reaction_types=(MessageReactionHandler.MESSAGE_REACTION_COUNT_UPDATED,)))
     return app
 
 async def post_init(app):
-    await app.bot.set_my_commands([BotCommand("start", "باز کردن منوی ربات"), BotCommand("stats", "آمار ربات (مدیر)")])
+    await app.bot.set_my_commands([BotCommand("start", "باز کردن پنل ربات")])
     await app.bot.set_chat_menu_button(menu_button=MenuButtonCommands())
 
 async def error_handler(update: object, context):
